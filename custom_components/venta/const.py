b8fb263db9d6014ce6a62aa5b3a81073310942e9
Michał Mrozek <mail@michalmrozek.dev>
2024-03-31 21:28:09 +0200

    fix!: mode level values

--- custom_components/venta/const.py
+++ custom_components/venta/const.py
@@ -13,11 +13,11 @@ NO_WATER_THRESHOLD = 50000
 
 MODE_SLEEP = "sleep"
 MODE_TURBO = "turbo"
-MODE_LEVEL_1 = "level 1"
-MODE_LEVEL_2 = "level 2"
-MODE_LEVEL_3 = "level 3"
-MODE_LEVEL_4 = "level 4"
-MODE_LEVEL_5 = "level 5"
+MODE_LEVEL_1 = "level_1"
+MODE_LEVEL_2 = "level_2"
+MODE_LEVEL_3 = "level_3"
+MODE_LEVEL_4 = "level_4"
+MODE_LEVEL_5 = "level_5"
 
 MODES_3: list[str] = [
     MODE_AUTO,
